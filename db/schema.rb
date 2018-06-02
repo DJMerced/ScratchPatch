09b842d63d4fd48b1680996c85eaefee11d9a8cb
David Merced <davidjames91@hotmail.com>
2018-06-02 06:39:45 -0400

    4.5 mess up

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180312144630) do
+ActiveRecord::Schema.define(version: 2018_06_02_025422) do
 
   create_table "orders", force: :cascade do |t|
     t.integer "user_id"
@@ -27,7 +27,6 @@ ActiveRecord::Schema.define(version: 20180312144630) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "colour"
-    t.string "price"
   end
 
 end
