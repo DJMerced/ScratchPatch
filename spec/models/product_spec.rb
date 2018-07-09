bb174bd628e117a0753723476472b0f17024d8e4
David Merced <davidjames91@hotmail.com>
2018-07-09 03:40:46 -0400

    6.5 and 6.4 DRY updates

--- spec/models/product_spec.rb
+++ spec/models/product_spec.rb
@@ -3,7 +3,7 @@ require 'rails_helper'
   describe Product do
     context "when product has comments" do
     let(:product) { Product.create!(name: "dog cast") }
-    let(:user) { User.create!(first_name: "Daisy", last_name: "Duke", email: "email@email.com", password: "password") }
+    let(:user) { FactoryBot.create(:user) }
     before do
       product.comments.create!(rating: 1, user: user, body: "Awful bike!")
       product.comments.create!(rating: 3, user: user, body: "Ok bike!")
